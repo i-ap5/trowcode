e3eef0f604f94bd7f772e0540bfa148729cf358c
Arun Krishna <arunpradeep585@gmail.com>
2026-01-23 23:16:30 +0530

    Worked on 4 d and build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import tailwindcss from '@tailwindcss/vite'; // <--- UPDATED THIS
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -10,7 +11,11 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react(),svgr() ],
+      plugins: [
+        react(),
+        svgr(), 
+        tailwindcss(), // This now works with the @tailwindcss/vite plugin
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
